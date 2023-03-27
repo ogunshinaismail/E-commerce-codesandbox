b003585b9cea5f281316f549fbe48a6ca8d644ba
Ogunneye <83877877+Tomtiko04@users.noreply.github.com>
2023-03-27 17:04:20 +0100

    changed the cart emoji

--- src/component/EmptyCart.jsx
+++ src/component/EmptyCart.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import './style/EmptyCart.css'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import './style/EmptyCart.css';
+import { useNavigate } from 'react-router-dom';
+import EmptyImg from '../images/empty.png';
 
 const EmptyCart = () => {
   const navigate = useNavigate();
   return (
+      
       <div className="cartempty-section">
-          <h1 className="cart-disapointmment-face">😞</h1>
+          <img src={EmptyImg} alt="..."/>
           <p>No product is found in your Cart.</p>
           <button onClick={() => {navigate('/product')}}>Shop Now</button>
       </div>
-  )
+  );
 }
 
 export default EmptyCart
