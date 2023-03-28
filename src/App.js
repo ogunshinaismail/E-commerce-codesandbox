d5a2eb5b2931ddbce9d37e38e975d36235ad3e2d
Ogunneye <83877877+Tomtiko04@users.noreply.github.com>
2023-03-28 21:18:47 +0100

    navbar fixed productdetail fixed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Router from "./routes/Router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
+// import RR from './images/trending-img.png'
 
 const App = () => {
   return (
